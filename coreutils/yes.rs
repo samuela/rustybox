32ca2f56260238c1dd7d1378928e752d545ed288
ZapAnton <wc3ft2@gmail.com>
2019-12-04 12:51:10 +0300

    coreutils/yes: Replaced the print! usage with the write_all

--- coreutils/yes.rs
+++ coreutils/yes.rs
@@ -8,16 +8,15 @@ use std::io::Write;
 #[no_mangle]
 pub unsafe extern "C" fn yes_main(mut _argc: c_int, mut _argv: *const *const c_char) -> c_int {
   let line = if env::args().count() > 1 {
-    env::args().collect::<Vec<String>>().join(" ")
+    env::args().skip(1).collect::<Vec<String>>().join(" ")
   } else {
     "y".to_string()
   };
+  let line = format!("{}\n", line);
   let mut stdout = io::stdout();
   loop {
-    print!("{}", line);
-    if let Err(_) = stdout.write_all(b"\n") {
-      break;
+    if let Err(_) = stdout.write_all(line.as_bytes()) {
+      return 1;
     }
   }
-  0
 }
