1f193d2b3c186169f3f1c221d513c98e5c4e22a3
Samuel Ainsworth <skainsworth@gmail.com>
2019-12-15 06:57:38 +0000

    link_funcs data_extract_to_stdout.rs

--- archival/libarchive/data_extract_to_stdout.rs
+++ archival/libarchive/data_extract_to_stdout.rs
@@ -1,15 +1,8 @@
 use crate::archival::libarchive::bb_archive::archive_handle_t;
 
-use libc;
-use libc::off_t;
-extern "C" {
-  #[no_mangle]
-  fn bb_copyfd_exact_size(fd1: libc::c_int, fd2: libc::c_int, size: off_t);
-}
-
 #[no_mangle]
 pub unsafe extern "C" fn data_extract_to_stdout(mut archive_handle: *mut archive_handle_t) {
-  bb_copyfd_exact_size(
+  crate::libbb::copyfd::bb_copyfd_exact_size(
     (*archive_handle).src_fd,
     1,
     (*(*archive_handle).file_header).size,
